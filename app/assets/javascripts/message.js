87de5df40b5b0cc015f3f49b9205c46b673a3feb
mitsugu3 <56537148+mitsugu3@users.noreply.github.com>
2020-02-22 20:14:53 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,83 @@
-$(function(){ 
-    function buildHTML(message){
-     if ( message.image ) {
-       var html =
-        `<div class="message" data-message-id=${message.id}>
-           <div class="upper-message">
-             <div class="upper-message__user-name">
-               ${message.user_name}
-             </div>
-             <div class="upper-message__date">
-               ${message.date}
-             </div>
-           </div>
-           <div class="lower-message">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-           <img src=${message.image} >
-         </div>`
-       return html;
-     } else {
-       var html =
-        `<div class="message" data-message-id=${message.id}>
-           <div class="upper-message">
-             <div class="upper-message__user-name">
-               ${message.user_name}
-             </div>
-             <div class="upper-message__date">
-               ${message.date}
-             </div>
-           </div>
-           <div class="lower-message">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-         </div>`
-       return html;
-     };
-   }
-$('.js-form').on('submit', function(e){
-   e.preventDefault();
-   var formData = new FormData(this);
-   var url = $(this).attr('action')
-   $.ajax({
-     url: url,
-     type: "POST",
-     data: formData,
-     dataType: 'json',
-     processData: false,
-     contentType: false
-   })
+$(function(){
+  function buildHTML(message){
+    var content = message.content ? `<p class="lower-message__content">${message.content}</p>` : "";
+    var image = message.image ? `<img class="lower-message__image" src="${message.image}">` : "";
+
+    var html = `<div class="message" data-id=${message.id}>
+                <div class="upper-message">
+                <div class="upper-message__user-name">
+                ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                ${message.created_at}
+                </div>
+                </div>
+                <div class="lower-message">
+                ${content}
+                ${image}
+                </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html); 
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');       
-       $('form')[0].reset();
+      $('.messages').append(html);
+      $('#new_message')[0].reset();
+      var height = $('.messages')[0].scrollHeight;
+      $('.messages').animate({scrollTop: height}, 500, 'swing');
     })
     .fail(function(){
-        alert('error');
-      });
-      return false;
+      alert('newMessageError');
+    })
+    return false;
+  });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data('id');
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+
+        if(message.id > last_message_id){
+        insertHTML += buildHTML(message);
+        }
+      })
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      var height = $('.messages')[0].scrollHeight;
+      $('.messages').animate({scrollTop: height}, 500, 'swing');
+    })
+    .fail(function() {
+      alert('reloadMessageError');
     });
+  };
+
+  $(window).on('load',function(){
+    if(document.URL.match("messages")) {
+      setInterval(reloadMessages, 5000);
+    }
+  });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
